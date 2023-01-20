0d2f52c433c9e5367b1b45d0a9489de9e0106db1
Leo Folsom <leoebfolsom@gmail.com>
2023-01-20 01:31:28 +0000

    couple more changes

--- models/beers/orders.sql
+++ models/beers/orders.sql
@@ -20,10 +20,10 @@ WITH generated_orders AS (
                    '{{ order_number }}'
              )::int                                                            AS order_no,
 
-             {{ randint(123456, 654321) }}                                  AS customer_id,
+             {{ randint(123455, 654321) }}                                  AS customer_id,
 
              {% if order_number is divisibleby 13 %}
-                'PENDING'                                                      AS status,
+                'PENDING!'                                                      AS status,
              {% else %}
                 'DELIVERED'                                                    AS status,
              {% endif %}
